403b5cdc6860d9a5e0f8ea15c70ed44fb0e6dfaa
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2017-10-17 18:06:38 +0100

    attempt to catch a known issue and give solution to user

--- setup.m
+++ setup.m
@@ -1,4 +1,4 @@
-close all; clc    
+close all; clc
 addpath([cd '/funcs'])
 addpath([cd '/funcs/export_fig'])
 addpath([cd '/funcs/acf'])
@@ -8,3 +8,10 @@ addpath([cd '/funcs/bensUtils'])
 addpath([cd '/funcs/bensPlotFunctions'])
 
 plot_formatting_setup
+
+% test for a known issue
+try
+    sum(log( binopdf(1, 2, 0.5 )));
+catch
+    error(sprintf('ERROR: It is likely that you have another binopdf function on your path. Use \n\t >> which binopdf\n\nto find where this is then use \n\t >> pathtool \n\nand move it down the list'))
+end
